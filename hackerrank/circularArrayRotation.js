d87c7cef6cfb19e3442e72f9add59adbdeddeba9
Emmy-github-webdev <ogahemmanuel75@gmail.com>
2022-02-15 13:05:51 +0000

    Update circular rotation

--- hackerrank/circularArrayRotation.js
+++ hackerrank/circularArrayRotation.js
@@ -2,10 +2,7 @@
 // Complete the circularArrayRotation function below.
 function circularArrayRotation(a, k, queries) {
     for(let i = 0; i<k; i++) {      
-        var x = a.pop()
-        a.unshift(x)
+       a.unshift(a.pop())
     }
-    for(let j = 0; j < queries.length; j++) {
-        console.log(a[queries[j]])
-    }
+   return queries.map(q=>a[q]);
 }
